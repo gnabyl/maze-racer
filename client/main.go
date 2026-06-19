9a5c4aab5d76801a0db72d54a809d658fbd7aba1
Bang NGUYEN <lelybang.nguyen@gmail.com>
2026-06-19 16:05:05 +0200

    feat: smart client and deterministic spectator colors
    - Client parses fog[12] wall bits, only sends valid moves
    - Client waits for state before sending next move
    - Spectator colors hashed from player ID, stable across reorders

--- client/main.go
+++ client/main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 
 	"github.com/gorilla/websocket"
 )
+
+const (
+	wallUp    = 1
+	wallDown  = 2
+	wallRight = 4
+	wallLeft  = 8
+)
+
+var dirWall = map[string]int{
+	"UP":    wallUp,
+	"DOWN":  wallDown,
+	"RIGHT": wallRight,
+	"LEFT":  wallLeft,
+}
+
+func validMoves(fog []int) []string {
+	cell := fog[12] // player always at center
+	var valid []string
+	for dir, wall := range dirWall {
+		if cell&wall == 0 {
+			valid = append(valid, dir)
+		}
+	}
+	return valid
+}
 
 func main() {
 	playerID := "player1"
@@ -23,11 +50,31 @@ func main() {
 
 	log.Printf("connected as %s", playerID)
 
+	rng := rand.New(rand.NewSource(int64(os.Getpid())))
+
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			log.Fatal("read error:", err)
 		}
-		fmt.Printf("server: %s\n", msg)
+
+		var resp struct {
+			Type string `json:"type"`
+			Fog  []int  `json:"fog"`
+		}
+		if err := json.Unmarshal(msg, &resp); err != nil || resp.Type != "state" {
+			continue
+		}
+
+		moves := validMoves(resp.Fog)
+		if len(moves) == 0 {
+			continue
+		}
+
+		dir := moves[rng.Intn(len(moves))]
+		payload, _ := json.Marshal(map[string]string{"move": dir})
+		if err := conn.WriteMessage(websocket.TextMessage, payload); err != nil {
+			log.Fatal("write error:", err)
+		}
 	}
 }
